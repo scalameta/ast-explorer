7f2704294bb635ab62bab749c54ff5e56b7886cf
Scala Steward <scala_steward@virtuslab.com>
2026-06-06 19:05:51 +0000

    Update scala3-library_sjs1 to 3.8.4

--- project.scala
+++ project.scala
@@ -1,6 +1,6 @@
 //> using platform scala-js
 //> using jsVersion 1.18.2
-//> using scala 3.8.2
+//> using scala 3.8.4
 //> using option -Wunused:all
 
 //> using dep org.scala-js::scalajs-dom::2.8.0
